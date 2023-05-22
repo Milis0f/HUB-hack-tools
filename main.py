5228e4c1f2c7355ecb0d7050d44f4a6e8673f62c
Milis0f <133584428+Milis0f@users.noreply.github.com>
2023-05-22 12:54:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-## importing the necessary packages
-import colorama
+import sys, time
+import os
+from rich import print
 
-print('''
-             ________________________________________________
-            /                                                |
-           |    _________________________________________     |
-           |   |                                         |    |
-           |   |  C:\> _                                 |    |
-           |   |           ''',colorama.Fore.GREEN +''' __  __ __  __ ____    '''+ colorama.Style.RESET_ALL,'''     |    |
-           |   |          ''',colorama.Fore.GREEN +''' / / / // / / // __ )   '''+ colorama.Style.RESET_ALL,'''     |    |
-           |   |         ''',colorama.Fore.GREEN +''' / /_/ // / / // __  |   '''+ colorama.Style.RESET_ALL,'''     |    |
-           |   |        ''',colorama.Fore.GREEN +''' / __  // /_/ // /_/ /    '''+ colorama.Style.RESET_ALL,'''     |    |
-           |   |       ''',colorama.Fore.GREEN +''' /_/ /_/ \____//_____/ ''',colorama.Fore.RED +'''v1'''+ colorama.Style.RESET_ALL,'''  '''+ colorama.Style.RESET_ALL,'''   |    |
-           |   |                                         |    |
-           |   |                                         |    |
-           |   |    ''',colorama.Fore.BLUE +'''[1] Tools:   '''+ colorama.Style.RESET_ALL,'''                       |    |
-           |   |    ''',colorama.Fore.BLUE +'''[2] Update:  '''+ colorama.Style.RESET_ALL,'''                       |    |
-           |   |    ''',colorama.Fore.BLUE +'''[3] About:   '''+ colorama.Style.RESET_ALL,'''                       |    |
-           |   |                                         |    |
-           |   |_________________________________________|    |
-           |                                                  |
-            \_________________________________________________/
-                   \___________________________________/
-                ___________________________________________
-             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
-          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
-       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
-    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
- _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
-:-------------------------------------------------------------------------:
-`---._.-------------------------------------------------------------._.---'      
-''')
+def print(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./500)
+
+
+print("Hello, [bold magenta]World[/bold magenta]!")
+def main():
+    print("╭──────────────────────────────────────────────────────────────────╮")
+    print("│      __  __ __  __ ____         │ Created by: Milis0f            │")
+    print("│     / / / // / / // __ )        │                                │")
+    print("│    / /_/ // / / // __  │        │                                │")
+    print("│   / __  // /_/ // /_/ /         │                                │")
+    print("│  /_/ /_/ \____//_____/ v1       │                                │")
+    print("│                                 │                                │")
+    print("│──────────────────────────────────────────────────────────────────│")
+    print("│                                                                  │")
+    print("│                                                                  │")
+    print("│                                                                  │")
+    print("│                                                                  │")
+    print("│                                                                  │")
+    print("│                                                                  │")
+    print("╰──────────────────────────────────────────────────────────────────╯")
+    try:
+      choice = int(input("Enter Your choice: "))
+    except (ValueError, EOFError, KeyboardInterrupt):
+      return print('\n[!] Interrupted! or Wrong Value')
+   
+    if choice not in range(4):
+      return('[!] Invalide Choice')
+   
+    if choice == 1:
+      os.system('clear')
+      os.system('bash -c "python3 /tools/tools.py"')
+      
+    elif choice == 2:
+      print('''[!] in building...''')
+      
+    elif choice == 3:
+      print('''[!] in building...''')
+      
+    else:
+      print("Invalide choice")
+    
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
